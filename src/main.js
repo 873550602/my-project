dfc59622f9ce98bbd26ec29ee00a2d883f21f754
873550602 <35795645+873550602@users.noreply.github.com>
2018-06-15 11:39:17 +0800

    'addVuex'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Vuex from 'vuex';
 import store from '@/store';
 import App from './App';
 import router from './router';
@@ -9,7 +8,6 @@ import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import '!style-loader!css-loader!less-loader!@/assets/css/com.less';
 Vue.use(iView)
-Vue.use(Vuex)
 
 Vue.config.productionTip = false;
 Vue.config.keyCodes.f1 = 112;
